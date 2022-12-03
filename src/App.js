f08eb909a3c59ae41bc85c672538f1ee0730a6cc
AbiyuTolosa <abiyunigussie7@gmail.com>
2022-12-03 20:50:47 +0300

    intergrated with dictionaryapi

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import SearchBar from "./components/SearchBar";
-import PageRoutes from "./PageRoutes";
+
 const App = () => {
   return (
     <div className="App">
-      <BrowserRouter>
-        <PageRoutes />
-      </BrowserRouter>
-      {/* <SearchBar /> */}
+      <SearchBar />
     </div>
   );
 };
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,30 +1,78 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { useState } from "react";
+import axios from "axios";
+
 const SearchBar = () => {
-  const [word, setWord] = useState("");
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    alert(`The word you entered was: ${word}`);
+  const [data, setData] = useState("");
+  const [searchWord, setSearchWord] = useState("");
+
+  const getMeaning = () => {
+    axios
+      .get(`https://api.dictionaryapi.dev/api/v2/entries/en/${searchWord}`)
+      .then((response) => {
+        setData(response.data[0]);
+        console.log(data);
+      });
+  };
+
+  const playAudio = () => {
+    const audioUrl = () => {
+      let url = "";
+      data.phonetics.map((item) => {
+        return item.audio !== "" ? (url = item.audio) : "";
+      });
+
+      return url;
+    };
+    console.log(audioUrl());
+    let audio = new Audio(audioUrl());
+    audio.play();
   };
 
   return (
     <div className="search-wrapper">
-      <form className="form-wrapper" onSubmit={handleSubmit}>
-        <div className="search-bar">
-          <input
-            type={"search"}
-            className="search-field"
-            placeholder="Search"
-            onChange={(e) => setWord(e.target.value)}
-          />
-        </div>
+      <div className="search-box">
+        <input
+          type={"search"}
+          className="search-field"
+          placeholder="Search"
+          onChange={(e) => {
+            setSearchWord(e.target.value);
+          }}
+        />
         <div className="search-btn-wrapper">
-          <button className="search-btn">
-            <i class="fa fa-search"></i>
+          <button
+            className="search-btn"
+            onClick={() => {
+              getMeaning();
+            }}
+          >
+            <i className="fa fa-search"></i>
           </button>
         </div>
-      </form>
+      </div>
+
+      {data && (
+        <div className="ShownResults">
+          <h2>
+            {data.word}{" "}
+            <button
+              className="speaker-btn"
+              onClick={() => {
+                playAudio();
+              }}
+            >
+              <i className="fa fa-volume-up speakerIcon"></i>
+            </button>
+          </h2>
+          <h4>Part of speech:</h4>
+          <p>{data.meanings[0].partOfSpeech}</p>
+          <h4>Definition:</h4>
+          <p>{data.meanings[0].definitions[0].definition}</p>
+          <h4>Examples:</h4>
+          <p>{data.meanings[0].definitions[0].example}</p>
+        </div>
+      )}
     </div>
   );
 };
